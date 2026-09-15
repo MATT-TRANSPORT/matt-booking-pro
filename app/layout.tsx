e68bd0a10af002920ac13edc833b4b3e9e2bb12e
MATT-TRANSPORT <kontakt@matt-transport.pl>
2026-09-15 21:04:54 +0200

    v4.7.0.1: load iOS mobile hotfix styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./v45.css";
 import "./customer-repeat.css";
+import "./ios-mobile-hotfix.css";
 import GrowthTracker from "@/components/GrowthTracker";
 
 export const metadata: Metadata = {
